e4271b7cb229ca1f449d987d6675c38f70d7a8be
AryanGh11 <108177164+AryanGh11@users.noreply.github.com>
2023-01-14 14:51:06 +0330

    Add files via upload

--- home.js
+++ home.js
@@ -19,6 +19,13 @@ const musicTrending4 = document.querySelector(".blame-music");
 const musicTrending5 = document.querySelector(".mafia-music");
 const musicTrending6 = document.querySelector(".lovely-music");
 
+const musicNews1 = document.querySelector(".sign-music");
+const musicNews2 = document.querySelector(".runaway-music");
+const musicNews3 = document.querySelector(".baby-music");
+const musicNews4 = document.querySelector(".lose-music");
+const musicNews5 = document.querySelector(".middle-music");
+const musicNews6 = document.querySelector(".tv-music");
+
 playBtnTrending1.addEventListener("click", () => {
     document.querySelectorAll("audio").forEach(el => el.pause());
     musicTrending1.play();
@@ -103,6 +110,87 @@ playBtnTrending6.addEventListener("click", () => {
         playBtn.style.display = "block";
     })
 });
-
-//news music should be okey
-
+playBtnNews1.addEventListener("click", () => {
+    document.querySelectorAll("audio").forEach(el => el.pause());
+    musicNews1.play();
+    const playBtn = document.querySelector(".play-btn-news1");
+    const pauseBtn = document.querySelector(".pause-btn7");
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    pauseBtn.style.right = "-5.8rem";
+    pauseBtn.addEventListener("click", () => {
+        musicNews1.pause();
+        pauseBtn.style.display = "none";
+        playBtn.style.display = "block";
+    })
+});
+playBtnNews2.addEventListener("click", () => {
+    document.querySelectorAll("audio").forEach(el => el.pause());
+    musicNews2.play();
+    const playBtn = document.querySelector(".play-btn-news2");
+    const pauseBtn = document.querySelector(".pause-btn8");
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    pauseBtn.style.right = "-5.8rem";
+    pauseBtn.addEventListener("click", () => {
+        musicNews2.pause();
+        pauseBtn.style.display = "none";
+        playBtn.style.display = "block";
+    })
+});
+playBtnNews3.addEventListener("click", () => {
+    document.querySelectorAll("audio").forEach(el => el.pause());
+    musicNews3.play();
+    const playBtn = document.querySelector(".play-btn-news3");
+    const pauseBtn = document.querySelector(".pause-btn9");
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    pauseBtn.style.right = "-5.8rem";
+    pauseBtn.addEventListener("click", () => {
+        musicNews3.pause();
+        pauseBtn.style.display = "none";
+        playBtn.style.display = "block";
+    })
+});
+playBtnNews4.addEventListener("click", () => {
+    document.querySelectorAll("audio").forEach(el => el.pause());
+    musicNews4.play();
+    const playBtn = document.querySelector(".play-btn-news4");
+    const pauseBtn = document.querySelector(".pause-btn10");
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    pauseBtn.style.right = "-5.8rem";
+    pauseBtn.addEventListener("click", () => {
+        musicNews4.pause();
+        pauseBtn.style.display = "none";
+        playBtn.style.display = "block";
+    })
+});
+playBtnNews5.addEventListener("click", () => {
+    document.querySelectorAll("audio").forEach(el => el.pause());
+    musicNews5.play();
+    const playBtn = document.querySelector(".play-btn-news5");
+    const pauseBtn = document.querySelector(".pause-btn11");
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    pauseBtn.style.right = "-5.8rem";
+    pauseBtn.addEventListener("click", () => {
+        musicNews5.pause();
+        pauseBtn.style.display = "none";
+        playBtn.style.display = "block";
+    })
+});
+playBtnNews6.addEventListener("click", () => {
+    document.querySelectorAll("audio").forEach(el => el.pause());
+    musicNews6.play();
+    const playBtn = document.querySelector(".play-btn-news6");
+    const pauseBtn = document.querySelector(".pause-btn12");
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    pauseBtn.style.right = "-5.8rem";
+    pauseBtn.addEventListener("click", () => {
+        musicNews6.pause();
+        pauseBtn.style.display = "none";
+        playBtn.style.display = "block";
+    })
+});
