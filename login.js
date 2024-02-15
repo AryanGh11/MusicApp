99b4696cc4a1854d75a9f03f945b91a1273f9e12
Aryan Gholami <mahdi.gholami875@gmail.com>
2024-02-15 20:58:25 +0330

    add a new feature

--- login.js
+++ login.js
@@ -3,38 +3,42 @@ const password = document.querySelector(".password");
 const login = document.querySelector(".login");
 const createAccount = document.querySelector(".create-account");
 const appLogo = document.querySelector(".logo-text");
+const appLogoImage = document.getElementById("app-logo");
+
+appLogoImage.addEventListener("click", () => {
+  window.location.href("web.telegram.org");
+});
 
 login.addEventListener("click", () => {
-    const usernameValue = username.value;
-    const passwordValue = password.value;
-    if(usernameValue === "" || passwordValue === ""){
-        swal({
-            title: "Empty informations",
-            text: "Please inter your username and password",
-            icon: "warning",
-            button: "Ok",
-            className: "swal-btn"
-        });
-    } 
-    if(usernameValue === "AryanGh" && passwordValue === "@AryanGholami11") {
-        window.location = "./home.html"
-    }
-    if(usernameValue === "user" && passwordValue === "123456") {
-        window.location = "./home.html"
-    }
-    if(usernameValue === "User" && passwordValue === "123456") {
-        window.location = "./home.html"
-    }
-    else {
-        swal({
-            title: "You don't have an account",
-            text: "Please create an account first, contact AryanGh",
-            icon: "warning",
-            button: "Ok",
-            className: "swal-btn"
-        });
-    }
-})
-appLogo.addEventListener("click", () => {
+  const usernameValue = username.value;
+  const passwordValue = password.value;
+  if (usernameValue === "" || passwordValue === "") {
+    swal({
+      title: "Empty informations",
+      text: "Please inter your username and password",
+      icon: "warning",
+      button: "Ok",
+      className: "swal-btn",
+    });
+  }
+  if (usernameValue === "AryanGh" && passwordValue === "@AryanGholami11") {
     window.location = "./home.html";
-})
+  }
+  if (usernameValue === "user" && passwordValue === "123456") {
+    window.location = "./home.html";
+  }
+  if (usernameValue === "User" && passwordValue === "123456") {
+    window.location = "./home.html";
+  } else {
+    swal({
+      title: "You don't have an account",
+      text: "Please create an account first, contact AryanGh",
+      icon: "warning",
+      button: "Ok",
+      className: "swal-btn",
+    });
+  }
+});
+appLogo.addEventListener("click", () => {
+  window.location = "./home.html";
+});
